1c7e91e18762b63e2612ca9e3dc06900d3c63601
Al Jadeed tech Labs <aljadeedtechlabs@gmail.com>
2026-03-31 19:11:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,4 +2,6 @@ self.addEventListener("install", (event) => {
   console.log("Service Worker Installed");
 });
 
-self.addEventListener("fetch", (event) => {});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
